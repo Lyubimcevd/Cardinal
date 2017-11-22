e4bd0ab6cc03be27a41372a09e8bad56845a1955
Любимцев Дмитрий Олегович <D:\ \thebat>
2017-11-22 16:46:57 +0300

    cardinal

---
+++ Cardinal/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Cardinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.NI = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 24;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(723, 244);
+            this.listBox1.TabIndex = 0;
+            // 
+            // NI
+            // 
+            this.NI.Text = "Диспетчер событий АСУП";
+            this.NI.Visible = true;
+            this.NI.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.NI_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 273);
+            this.Controls.Add(this.listBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "События";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.NotifyIcon NI;
+    }
+}
+
---
+++ Cardinal/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Windows.Threading;
+
+namespace Cardinal
+{
+    public partial class Form1 : Form
+    {
+        SqlDependency sqlDependencyStalker;
+        SqlConnection conn;
+        SqlCommand SC;
+        SqlDataReader SDR;
+        Dispatcher Dis = Dispatcher.CurrentDispatcher;
+        string log_caption, group, str_connect = "user id=ldo;password=IfLyyz4sCJ;server=nitel-hp;database=uit;MultipleActiveResultSets=True";
+        bool is_visual = false;
+        string[] groups_and_users;
+
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(str_connect);
+            conn.Open();
+            SqlDependency.Start(str_connect);
+            Stalker();
+            NI.Icon = Cardinal.Properties.Resources.eye_alien;
+        }
+
+        void Stalker()
+        {
+            if (sqlDependencyStalker != null)
+            {
+                sqlDependencyStalker.OnChange -= OnDatabaseChange_Id_and_nom_ceh;
+                sqlDependencyStalker = null;
+            }
+            using (var command = new SqlCommand("select naim from dbo.asup_log", conn))
+            {
+                sqlDependencyStalker = new SqlDependency(command);
+                sqlDependencyStalker.OnChange += new OnChangeEventHandler(OnDatabaseChange_Id_and_nom_ceh);
+                command.ExecuteReader();
+            }
+        }
+
+        private void NI_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Dis.Invoke(new Action(FormVisible));
+        }
+
+        protected override void OnShown(System.EventArgs e)
+        {
+            base.OnShown(e);
+            Hide();
+        }
+
+        void OnDatabaseChange_Id_and_nom_ceh(object sender, SqlNotificationEventArgs e)
+        {
+            SC = new SqlCommand("select top 1 * from dbo.asup_log order by dt desc", conn);
+            SDR = SC.ExecuteReader();
+            while (SDR.Read())
+            {
+                log_caption = SDR.GetDateTime(0).ToShortTimeString() + "  " + SDR.GetString(1);
+                if (!SDR.IsDBNull(2)) group = SDR.GetString(2);
+                else group = "";
+            }
+            if (group.Trim().Length != 0)
+            {
+                groups_and_users = group.Split(',');
+                for (int i = 0; i < groups_and_users.Length; i++)
+                {
+                    SC = new SqlCommand("select * from cvodka.dbo.log_asup_ where vxod is null and loginn = '" + groups_and_users[i] + "'", conn);
+                    SDR = SC.ExecuteReader();
+                    if (SDR.HasRows)
+                    {
+                        SC = new SqlCommand("select * from (select l2.loginn as groupn,l.loginn as polz from [cvodka].[dbo].[group_users] gu inner join "
+                                            + "cvodka.dbo.log_asup_ l on l.id=gu.id_u inner join cvodka.dbo.log_asup_ l2 on l2.id=gu.id_g) as tmp where"
+                                            + " groupn = '" + groups_and_users[i] + "' and polz = '" + Environment.UserName + "'", conn);
+                        SDR = SC.ExecuteReader();
+                        if (SDR.HasRows) is_visual = true;
+                    }
+                    else
+                    if (Environment.UserName == groups_and_users[i]) is_visual = true;
+                }
+            }
+            else is_visual = true;
+            if (is_visual)
+            {
+                Dis.Invoke(new Action(FormVisible));
+                Dis.Invoke(new Action(AddInList));
+                Stalker();
+            }
+            is_visual = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason != CloseReason.WindowsShutDown)
+            {
+                e.Cancel = true;
+                Dis.Invoke(new Action(FormInvisible));
+            }
+        }
+
+        void FormVisible()
+        {
+            Visible = true;
+            NI.Visible = false;
+        }
+
+        void FormInvisible()
+        {
+            Visible = false;
+            NI.Visible = true;
+        }
+       
+        void AddInList()
+        {
+            listBox1.Items.Add(log_caption);
+        }
+
+    }
+}
